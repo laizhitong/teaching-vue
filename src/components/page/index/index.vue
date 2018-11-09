7b8770dffab0e8a6d797bdc54ad22fb8c5dec8a6
laizhitong <15075669582@163.com>
2018-11-09 15:02:08 +0800

    f

--- src/components/page/index/index.vue
+++ src/components/page/index/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <header class="header">
-    	<div class="back iconfont">&#xe624;</div>
-    	<div class="search">
-    		<a href="#" class="jump">
-    			<i class="iconfont">&#xe86e;</i>
-    			<span>输入城市/景点/游玩主题</span>
-    		</a>
-    	</div>
+		<div class="back iconfont">&#xe624;</div>
+		<div class="search">
+			<a href="#" class="jump">
+				<i class="iconfont">&#xe86e;</i>
+				<span>输入城市/景点/游玩主题</span>
+			</a>
+		</div>
     	<div class="city">
     		<span>城市</span>
     		<i class="icon iconfont">&#xe79b;</i>
@@ -21,15 +21,16 @@
 	  	</swiper-slide>
 	    <div class="swiper-pagination"  slot="pagination"></div>
 	  </swiper>
-	  <swiper>
+	  <swiper :options="iconsOption">
 	    <swiper-slide v-for="(pageInfo, index) in pages" :key="index" class="icon-wrapper">
 	    	<div v-for="item in pageInfo" :key="item.id" class="icon-item">
 	    		<div class="icon-img-con">
 	    			<img class="icon-img" :src="item.imgUrl">
-	    			<span>{{item.title}}</span>
+	    			<div>{{item.title}}</div>
 	    		</div>
 	    	</div>
 	  	</swiper-slide>
+	  	<div class="swiper-pagination"  slot="pagination"></div>
 	  </swiper>
   </div>
 </template>
@@ -42,15 +43,17 @@ export default {
       	swiperInfo: [],
       	iconsInfo: [],
         swiperOption: {
-        	 autoplay:true,
-        	 direction: 'horizontal',
-        	 loop:true,
-        	 pagination:{
+        	autoplay:true,
+        	direction: 'horizontal',
+        	loop:true,
+        	pagination:{
+        		el:'.swiper-pagination'
+        	}
+        },
+        iconsOption: {
+        	pagination:{
         	 	el:'.swiper-pagination'
-        	 }
-          // some swiper options/callbacks
-          // 所有的参数同 swiper 官方 api 参数
-          // ...
+        	}
         }
       }
     },
@@ -143,21 +146,31 @@ export default {
 	.swiper-img {
 		width: 100%;
 	}
-	.icon-wrapper {
-
+	.icon-wrapper  {
+		padding-top: .1rem;
 	}
 	.icon-item {
 		box-sizing: border-box;
 		float: left;
 		width: 25%;
-		padding: .4rem;
+	
+		padding-top: .1rem;
+		text-align: center;
 	}
 	.icon-img-con {
 		width: 100%;
 		height: 0;
-		padding-bottom: 100%;
+		padding-bottom: 80%;
 	}
 	.icon-img {
-		width: 100%;
+		width: 1.1rem;
+		height: 1.1rem;
+	}
+	.icon-img-con div{
+		margin-top: .1rem;
+	}
+	.icon-wrapper .swiper-pagination{
+		position: absolute;
+		bottom: -10px;
 	}
 </style>
